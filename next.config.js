5d0bf936d72f81cc1a64f2b4157ab333eb94333f
Feustey <stephane@token-for-good.com>
2025-03-30 10:31:02 +0200

    Fix: Correction des problèmes de style et d'affichage du logo SVG en modifiant la configuration de Next.js et Netlify

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['mcp-c544a464bb52.herokuapp.com'],
   },
-  output: 'export',
   reactStrictMode: false,
   webpack: (config) => {
     config.resolve.alias = {
