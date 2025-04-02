aad05ae6f340f223a5fd5f9e1934d9bf555822eb
Feustey <stephane@token-for-good.com>
2025-04-02 15:46:36 +0200

    feat: alias @ supr

--- next.config.js
+++ next.config.js
@@ -18,7 +18,6 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(process.cwd(), "app"),
       "@components": path.resolve(process.cwd(), "app/components"),
       "@lib": path.resolve(process.cwd(), "app/lib"),
       "@models": path.resolve(process.cwd(), "app/models"),
