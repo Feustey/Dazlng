55192a5f1ba84a5e5b2d57367e88149447c4248a
Feustey <stephane@token-for-good.com>
2025-04-03 12:25:52 +0200

    Fix: U un aliasut

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout"; // Utilisation de l'alias
+import Layout from "./components/Layout";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   // Retourner directement les enfants, le layout global s'occupe de la structure
