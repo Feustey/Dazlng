e2119fb315974e50f2a18e3ce4df770d517cb46e
Feustey <stephane@token-for-good.com>
2025-04-04 03:29:30 +0200

    fix: amélioration de la gestion des locales dans le middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,36 @@
 import createMiddleware from "next-intl/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export default createMiddleware({
-  locales: ["en", "fr"],
-  defaultLocale: "en",
-  localePrefix: "always",
-});
+const locales = ["en", "fr"];
+const defaultLocale = "en";
+
+export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
+  );
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    const locale = defaultLocale;
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+  }
+
+  // Check if the locale is valid
+  const pathnameLocale = pathname.split("/")[1];
+  if (!locales.includes(pathnameLocale)) {
+    return NextResponse.redirect(
+      new URL(`/${defaultLocale}${pathname}`, request.url)
+    );
+  }
+
+  return createMiddleware({
+    locales,
+    defaultLocale,
+    localePrefix: "always",
+  })(request);
+}
 
 export const config = {
   matcher: [
